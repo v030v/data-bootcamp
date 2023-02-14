27e1a23d0a39fb2e2a4eff664ec0178383398715
v030v <pjeongin25@gmail.com>
2023-02-14 12:36:27 +0900

    day2

---
+++ 20230214.py
@@ -0,0 +1,32 @@
+# a = int(input())
+# print(a) #int 없으면 str
+
+# distance = int(input())
+# if distance <= 3 :
+#     print ('걸어가세요')
+# elif 3 < distance < 10 :
+#     print ('버스타세요')
+# else :
+#     print ('택시타세요')
+
+# for y in range (1900, 2101) :
+#     if y % 4 == 0 and y % 100 != 0 or y % 400 == 0 :
+#         print(y)
+
+# def func_1(year) :
+#     if year % 4 == 0 and year % 100 != 0 or year % 400 == 0 :
+#         return ("윤년임")
+#     else :
+#         return ("평년임")
+
+# total = []
+# for x in range(1,5) :
+#     total.append(x) 
+# total = [x for x in range(1,5)] #list comprehension
+
+# total = []
+# for x in range(1,100) :
+#     if x % 3 == 0 and x % 5 == 0 :
+#         total.append(x)
+
+# total12 = [x for x in range(1,100) if x % 3 == 0 and x % 5 == 0]
